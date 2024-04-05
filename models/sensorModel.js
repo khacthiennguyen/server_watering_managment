1f1d551d07729295f47f253ddd950c217a8d4b36
your name <khacthien.brand@gmail.com>
2024-04-05 22:02:17 +0700

    fix telex model

--- models/sensorModel.js
+++ models/sensorModel.js
@@ -1,39 +1,32 @@
-const mongoose = require("mongoose")
- const sensorSchema = new mongoose.Schema({
-    sensorId:{
-        type:String,
-        required: [true],
+const mongoose = require("mongoose");
+const sensorSchema = new mongoose.Schema(
+  {
+    sensorId: {
+      type: String,
+      required: [true],
+    },
+    day: {
+      type: String,
+      required: [true],
+    },
+    time: {
+      type: String,
+      required: [true],
+    },
+    moisture: {
+      type: String,
+      required: [true],
+    },
+    location: {
+      type: String,
+      required: [true],
+    },
+    activate: {
+      type: String,
+      required: [true],
+    },
+  },
+  { timestamps: true }
+);
 
-    },
-    day:{
-        type:String,
-        required: [true],
-    },
-    time:{
-        type:String,
-        required: [true],
-    },
-    moisture:{
-        type:String,
-        required: [true],
-
-    },
-    location:{
-        type:String,
-        required: [true],
-    },
-<<<<<<< HEAD
-    activate:{
-        type:String,
-        required: [true],
-    }
-=======
-  activate:{
-   type:String,
-        required: [true],
-  }
->>>>>>> 4b9b9a368a58a4a7e8f75e5f360720e76a1b7f67
-
- },{timestamps:true})
-
- module.exports = mongoose.model('sensor',sensorSchema)
+module.exports = mongoose.model("sensor", sensorSchema);
